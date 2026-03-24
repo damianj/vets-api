81f7ddf0a8abc92bb150169c2a0d6f7fd1c2fe80
Thomas Krenzke <196683837+thomaskrenzke@users.noreply.github.com>
2026-03-24 11:19:27 -0700

    make sure 0976 and 0989 have prefill enabled and working (#27209)

--- spec/models/form_profiles/va0976_spec.rb
+++ spec/models/form_profiles/va0976_spec.rb
@@ -10,4 +10,23 @@ RSpec.describe FormProfiles::VA0976 do
       expect(profile.metadata).to eq({ version: 0, prefill: true, returnUrl: '/applicant/information' })
     end
   end
+
+  describe '#prefill' do
+    before do
+      allow_any_instance_of(BGS::People::Service).to(
+        receive(:find_person_by_participant_id).and_return(BGS::People::Response.new({ file_nbr: '123456789' }))
+      )
+      allow_any_instance_of(User).to(
+        receive(:participant_id).and_return('600061742')
+      )
+    end
+
+    it 'prefills form data' do
+      VCR.use_cassette('lighthouse/direct_deposit/show/200_valid_new_icn') do
+        data = profile.prefill
+        expect(data[:form_data]['applicantName']).to eq({ 'first' => 'Abraham',
+                                                          'last' => 'Lincoln' })
+      end
+    end
+  end
 end
