a87e49fb454ce14b6d419e6479d508327251e56d
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2026-04-01 08:38:21 -0600

    Fix flaky VASS configuration spec caused by CopHelper name collision (#27476)

--- modules/vass/spec/services/vass/configuration_spec.rb
+++ modules/vass/spec/services/vass/configuration_spec.rb
@@ -4,18 +4,20 @@ require 'rails_helper'
 require_relative '../../support/vass_settings_helper'
 
 RSpec.describe Vass::Configuration do
-  let(:configuration) { described_class.instance }
+  # Use `vass_config` instead of `configuration` to avoid collision with
+  # CopHelper#configuration (from rubocop/rspec/support) which returns RuboCop::Config.
+  let(:vass_config) { described_class.instance }
   let(:oauth_url) { 'https://login.microsoftonline.example/oauth2/token' }
 
   before do
-    configuration.send(:instance_variable_set, :@__oauth_conn_pool__, nil)
+    vass_config.send(:instance_variable_set, :@__oauth_conn_pool__, nil)
   end
 
   describe '#oauth_connection' do
     it 'uses Settings.vass.oauth_open_timeout and oauth_read_timeout on the Faraday connection' do
       stub_vass_settings(oauth_open_timeout: 12, oauth_read_timeout: 34)
 
-      conn = configuration.oauth_connection(server_url: oauth_url)
+      conn = vass_config.oauth_connection(server_url: oauth_url)
 
       expect(conn.options.open_timeout).to eq(12)
       expect(conn.options.timeout).to eq(34)
@@ -24,14 +26,14 @@ RSpec.describe Vass::Configuration do
     it 'reflects updated Settings when a new connection is built' do
       stub_vass_settings(oauth_open_timeout: 7, oauth_read_timeout: 21)
 
-      first = configuration.oauth_connection(server_url: oauth_url)
+      first = vass_config.oauth_connection(server_url: oauth_url)
       expect(first.options.open_timeout).to eq(7)
       expect(first.options.timeout).to eq(21)
 
-      configuration.send(:instance_variable_set, :@__oauth_conn_pool__, nil)
+      vass_config.send(:instance_variable_set, :@__oauth_conn_pool__, nil)
       stub_vass_settings(oauth_open_timeout: 8, oauth_read_timeout: 22)
 
-      second = configuration.oauth_connection(server_url: oauth_url)
+      second = vass_config.oauth_connection(server_url: oauth_url)
       expect(second.options.open_timeout).to eq(8)
       expect(second.options.timeout).to eq(22)
     end
